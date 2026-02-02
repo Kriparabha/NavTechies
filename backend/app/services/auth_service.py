fb25bcb2372d6cc9b4b51956e3521667af0efbfb
Anirudhya Das <131817827+AnirudhyaDas@users.noreply.github.com>
2026-02-03 00:26:40 +0530

    Add files via upload

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,7 +2,16 @@ from typing import Optional, Dict, Any
 from datetime import datetime, timedelta
 import jwt
 from backend.app.core.config import settings
+from passlib.context import CryptContext
 
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
 class AuthService:
     def __init__(self):
